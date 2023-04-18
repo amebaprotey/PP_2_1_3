b2d0b2f01a44e52a4709e1664b6a5652c2570702
hrulev <amebaprotey@rambler.ru>
2023-04-19 02:46:26 +0300

    commit1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public String getAge() {
+        return name;
+    }
+
+    public void setAge(String name) {
+        this.name = name;
+    }
+}
